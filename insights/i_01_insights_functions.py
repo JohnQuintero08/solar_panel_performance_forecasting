ec8cc5ea0fea1837ee2cbbf0cb814f2c3e7f74f6
John_Quintero <john08quinteroc@gmail.com>
2025-06-10 15:07:56 -0500

    EDA and insight functions

--- insights/i_01_insights_functions.py
+++ insights/i_01_insights_functions.py
@@ -56,22 +56,26 @@ def plot_scatter_trim_by_year(df, param, year=1999):
     plt.xlabel('Year')
     plt.show()
 
-def plot_seasonal_param(df, param, year=1999):
+def plot_seasonal_param(df, param, year_from=2000, year_to=2025):
     plt.figure(figsize=general_plot_size)
-    sns.lineplot(data=df.query(f'year > {year}') ,
+    sns.lineplot(data=df.query(f'year >= {year_from} and year <= {year_to}') ,
                  x='month', 
                  y=param, 
                  hue='year', 
                  palette='deep',
                  errorbar=None #Intervalo de confianza
                  )
+    plt.title(f'Monthly seasonal behaviour of {param.capitalize()} from {year_from} to {year_to}')
+    plt.ylabel(param.capitalize())
+    plt.xlabel('Month')
+    plt.tight_layout()
     plt.show()
 
 
-def plot_seasonal_param_polar_trim(df, param, year=1999):
+def plot_seasonal_param_polar_trim(df, param, year_from=2000, year_to=2025):
     plt.figure(figsize=(8, 8))
     ax = plt.subplot(111, polar=True)
-    years = df.query(f'year > {year}')['year'].unique()
+    years = df.query(f'year >= {year_from} and year <= {year_to}')['year'].unique()
 
     for i in sorted(years):
         sub = df[df['year'] == i]
@@ -97,11 +101,11 @@ def plot_seasonal_param_polar_trim(df, param, year=1999):
     plt.show()
 
 
-def multi_lag_plot_trim(df, param, year=1999):    
+def multi_lag_plot_trim(df, param, year_from=2000, year_to=2025):    
     fig, axs = plt.subplots(3,4, figsize=(15,9))
     axs = axs.flatten()
     for i, ax in enumerate(axs):
-        pd.plotting.lag_plot(df.query(f'year > {year}')[param], lag=(i+1)*180,ax=ax)
+        pd.plotting.lag_plot(df.query(f'year >= {year_from} and year <= {year_to}')[param], lag=(i+1)*180,ax=ax)
         ax.set_title(f"Lag {(i + 1)*180}")
     plt.title(f'Lag plot of {param}')
     plt.tight_layout()
@@ -110,35 +114,22 @@ def multi_lag_plot_trim(df, param, year=1999):
 def lag_plot_trim(df, param, lag):
     fig, ax = plt.subplots(figsize=(16,5))
     plot_acf(df[param], lags=lag,ax=ax)
+    plt.title(f'Autocorrelation / Lag plot of {param.capitalize()}')
+    plt.xlabel(param.capitalize())
+    plt.ylabel('Autocorrelation')
+    plt.tight_layout()
     plt.show()
 
-def plot_box(df, param):
+def plot_box(df, param, year_from, year_to, split_year =2012):
     fig, axs = plt.subplots(1,2, figsize=general_plot_size)
-    sns.boxplot(data = df.query('year <= 2012'), x='month', y=param, ax=axs[0])
-    sns.boxplot(data = df.query('year > 2012'), x='month', y=param, ax=axs[1])
+    sns.boxplot(data = df.query(f'year <= {split_year}'), x='month', y=param, ax=axs[0])
+    sns.boxplot(data = df.query(f'year > {split_year}'), x='month', y=param, ax=axs[1])
+    axs[0].set_title(f'Boxplot of {param.capitalize()} from {year_from} to {split_year}')
+    axs[1].set_title(f'Boxplot of {param.capitalize()} from {split_year} to {year_to}')
+    axs[0].set_xlabel('Month')
+    axs[1].set_xlabel('Month')
+    axs[0].set_ylabel(param.capitalize())
+    axs[1].set_ylabel(param.capitalize())   
+    plt.tight_layout()
     plt.show()
     
-    
-# IMPORTANTEEEEE
-# # - There is an irradiation data of -999, which is impossible, because the measure is greater than 0
-# TODO CREAR UNA FUNCIÓN QUE ELIMINE LOS DATOS QUE TENGAN VELOCIDAD MENOR A CERO, IRRADIACION MENOR A CERO
-df = pd.read_feather('data/preprocess_data.feather')
-df= df.query('irradiation > 0')
-
-plot_variable_hist(df, 'irradiation')
-new_df = create_time_features(df)
-plot_scatter_trim_by_year(new_df,'precipitation',2012)
-plot_seasonal_param(new_df,'irradiation', 2012)
-plot_seasonal_param_polar_trim(new_df, 'irradiation', 2012)
-multi_lag_plot_trim(new_df, 'irradiation', 2012)
-lag_plot_trim(new_df, 'irradiation', 365)
-plot_box(new_df, 'irradiation')
-
-
-
-
-
-
-
-
-
